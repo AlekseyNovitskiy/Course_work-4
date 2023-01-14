c1184f2e135be55983dc396a44196cc193aab987
AlekseyNovitskiy <novickii_aleksei@mail.ru>
2023-01-14 18:10:28 +0300

    Билет 2. Задача.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println(isPalindrome("ТРОС И СОРТ"));
     }
+
     public static boolean isPalindrome (String text) {
         text = text.replaceAll("\\W","");//удаляем все ненужное
         StringBuilder strBuilder = new StringBuilder(text);
@@ -13,4 +15,5 @@ public class Main {
         return text.equalsIgnoreCase(invertedText) ;//возвращаем сравнение двух строк вне зависимости от регистра
 
     }
+
 }
